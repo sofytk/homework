50d6654dd956331989665315d538e0c4ea793d44
sofytk <sofata31@gmail.com>
2022-09-29 19:11:47 +0300

    homework2

--- src/task4.java
+++ src/task4.java
@@ -4,7 +4,7 @@ public class task4 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         float x = in.nextFloat();
-        if ((x<=-2 && x<=3) | (x>=6 && x<=9)){
+        if ((x<=-2 && x<=3) || (x>=6 && x<=9)){
             System.out.println("false");
         }
         else{
