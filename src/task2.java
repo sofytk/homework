8b2f625a39e55c6e84c2faa2c43196098b0bfa77
sofytk <sofata31@gmail.com>
2022-09-30 19:15:08 +0300

    homework2

--- src/task2.java
+++ src/task2.java
@@ -7,21 +7,22 @@ public class task2 {
         int rabb = 11;
         int wolf = 2;
         int plusw = 0;
-        //int zoo = 1;
-        for (int i = 1; i < n+1 ; ++i) {
-            switch (i%2) {
-                case 0:
-                    rabb -= 10;
-                    ++plusw;
-                    break;
-                case 1:
-                    rabb *= 3;
-                    break;
-            }
-            if (rabb<10) --wolf;
-            if (plusw == 7) ++wolf;
-        }
-        System.out.println(rabb + " "+ wolf);
+        for (int i = 1; i < n + 1; ++i) {
+               switch (i % 2) {
+                   case 0:
+                       rabb -= 10;
+                       ++plusw;
+
+                       break;
+                   case 1:
+                       rabb *= 3;
+                       break;
+               }
+            if (rabb < 10 && rabb>0) --wolf;
+            if (plusw % 70 == 0) ++wolf;
+           }
+           System.out.println(rabb + " " + wolf);
+
 
     }
 
