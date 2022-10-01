874aafcc7c4d2ead42e806c3959c9c75068d79e9
sofytk <sofata31@gmail.com>
2022-10-01 18:23:28 +0300

    homework3

--- src/task1.java
+++ src/task1.java
@@ -1,21 +1,57 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int num = in.nextInt();
-        for (int i = 1; ; i++) {
-            if (n%10 == num){
-                System.out.println( n/(Math.pow(10, i)) );
-                System.out.println(n%(Math.pow(10, i-1)));
-                break;
+        int h = in.nextInt();
+
+        String ind = "";
+        String num = String.valueOf(n);
+        int ans = 0;
+
+//        for (int i = 0; i <= num; i++) {
+//            if (n%10 == h){
+//                ind = i;
+//            }
+//            n /= 10;
+//        }
+
+        for (int i =0; i < num.length(); i++) {
+            if (n % 10 != h){
+                ans += n%10 * (int)(Math.pow(10, i)) ;
+                //System.out.println(ans);
             }
-            else {
-                n /= 10;
-               // System.out.println(n);
+            n /= 10;
+        }
+
+
+
+
+        for (char digit : num.toCharArray()) { // cycle through every char
+            if(digit!='0') { // if it is not a zero
+                ind+=digit; // append it to the resultString
             }
         }
+       // if (!ind.equals("")) {
+            System.out.println(Integer.parseInt(ind));
+
+
+
+
+
+
+//        if (ans!=0 && ans != num-1){
+//            System.out.println((int)(ab / Math.pow(10, ans+1) )  + "" +  (int)(ab % Math.pow(10, ans) ) );
+//        }
+//        else if (ans == num-1){
+//            System.out.println((int)(ab % Math.pow(10, num-1) ));
+//        }
+//        else{
+//            System.out.println(ab / 10);
+//        }
 
     }
 }
+
--- src/task4.java
+++ src/task4.java
@@ -1,39 +1,39 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class task3 {
-    //Способ с массивом
+public class task4 {
+    // Способ с массивами
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] array = new int[n];
         for (int i = 0; i < n; i+=1) {
             array[i] = in.nextInt();
         }
-        int min = array[0];
-        for (int i = 0; i < array.length ; i++) {
-            if (array[i] < array[0] && array[i]>=0) {
-                min = array[i];
+
+        for (int i = 1; i < array.length ; i++) {
+            int m = array[i-1];
+            if (m < array[i]) {
+                System.out.print(array[i] + " ");
+
             }
         }
-        System.out.println(min);
     }
 //Способ без массивов
-
 //    public static void main(String[] args) {
 //        Scanner in = new Scanner(System.in);
 //        int len = in.nextInt();
-//        int n = 0;
-//        int min = 0;
-//        for (int i = 0; i < len; i++) {
+//        int n = 1;
+//        int min;
+//        for (int i = 0; i<len; i++) {
 //            min = n;
 //            n = in.nextInt();
-//            if (n < min && n>=0) min = n;
+//            if (min < n) {
+//                System.out.print(n + " ");
+//            }
 //        }
-//        System.out.println(min);
-//
 //    }
+
+
+
 }
-
-
-
-
